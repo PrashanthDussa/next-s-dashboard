11f82529c77bfd72f416af94098a3490bb080b15
Prashanth Dussa <prashanth.dussa05@gmail.com>
2024-06-24 19:43:11 -0500

    modified

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import { UpdateInvoice, DeleteInvoice } from '@/app/ui/invoices/buttons';
 import InvoiceStatus from '@/app/ui/invoices/status';
 import { formatDateToLocal, formatCurrency } from '@/app/lib/utils';
 import { fetchFilteredInvoices } from '@/app/lib/data';
@@ -45,10 +44,6 @@ export default async function InvoicesTable({
                       {formatCurrency(invoice.amount)}
                     </p>
                     <p>{formatDateToLocal(invoice.date)}</p>
-                  </div>
-                  <div className="flex justify-end gap-2">
-                    <UpdateInvoice id={invoice.id} />
-                    <DeleteInvoice id={invoice.id} />
                   </div>
                 </div>
               </div>
@@ -109,8 +104,6 @@ export default async function InvoicesTable({
                   </td>
                   <td className="whitespace-nowrap py-3 pl-6 pr-3">
                     <div className="flex justify-end gap-3">
-                      <UpdateInvoice id={invoice.id} />
-                      <DeleteInvoice id={invoice.id} />
                     </div>
                   </td>
                 </tr>
